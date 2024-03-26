1e1cb6361aa3c6e2f46f462908ca5be299656b51
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2024-03-27 00:28:21 +0100

    Created server and test modules (#3)

---
+++ konfigyr-server/src/main/java/com/konfigyr/KonfigyrApplication.java
@@ -0,0 +1,19 @@
+package com.konfigyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Konfigyr Spring server application main class.
+ *
+ * @author : Vladimir Spasic
+ * @since : 26.03.24, Tue
+ **/
+@SpringBootApplication
+public class KonfigyrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonfigyrApplication.class, args);
+	}
+
+}
