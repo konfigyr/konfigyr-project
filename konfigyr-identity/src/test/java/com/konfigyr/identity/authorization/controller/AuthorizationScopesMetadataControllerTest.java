a9e97a0fe5442f2f8527f2635667ab6417d521df
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2026-03-04 15:15:08 +0100

    Fix scope metadata controller test

--- konfigyr-identity/src/test/java/com/konfigyr/identity/authorization/controller/AuthorizationScopesMetadataControllerTest.java
+++ konfigyr-identity/src/test/java/com/konfigyr/identity/authorization/controller/AuthorizationScopesMetadataControllerTest.java
@@ -12,10 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
@@ -29,31 +28,31 @@ class AuthorizationScopesMetadataControllerTest {
 	MockMvcTester mvc;
 
 	@Test
-	@DisplayName("should redirect to login page when not authenticated")
+	@DisplayName("should render registered OAuth scope metadata when not authenticated")
 	void shouldRedirectToLogin() {
-		final var request = get("/oauth/scope-metadata");
-
-		assertThat(mvc.perform(request))
-				.apply(log())
-				.hasStatus3xxRedirection()
-				.hasRedirectedUrl("/login");
+		assertScopeMetadata(get("/oauth/scope-metadata"));
 	}
 
 	@Test
-	@DisplayName("should render registered OAuth scope metadata")
+	@DisplayName("should render registered OAuth scope metadata when authenticated")
 	void shouldFailToFindRegisteredOAuthClient() {
-		final var request = get("/oauth/scope-metadata")
-				.with(authenticated());
+		assertScopeMetadata(
+				get("/oauth/scope-metadata")
+						.with(SecurityMockMvcRequestPostProcessors.jwt())
+		);
+	}
 
+	void assertScopeMetadata(RequestBuilder request) {
 		mvc.perform(request)
 				.assertThat()
+				.apply(log())
 				.hasStatusOk()
 				.bodyJson()
 				.convertTo(InstanceOfAssertFactories.iterable(ScopeMetadata.class))
 				.containsExactlyInAnyOrder(
 						new ScopeMetadata(
 								OAuthScope.OPENID.getAuthority(),
-						"Scope used to indicate that the application intends to use OIDC to verify the user's identity."
+								"Scope used to indicate that the application intends to use OIDC to verify the user's identity."
 						),
 						new ScopeMetadata(
 								OAuthScope.NAMESPACES.getAuthority(),
@@ -74,10 +73,6 @@ class AuthorizationScopesMetadataControllerTest {
 						new ScopeMetadata(OAuthScope.WRITE_PROFILES.getAuthority(), "Grants read and write access to profile."),
 						new ScopeMetadata(OAuthScope.DELETE_PROFILES.getAuthority(), "Grants read, write and delete access to profile.")
 				);
-	}
-
-	static RequestPostProcessor authenticated() {
-		return SecurityMockMvcRequestPostProcessors.jwt();
 	}
 
 }
