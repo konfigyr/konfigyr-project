b3ac032e7514fa9aae384c6d53e6d1452cd527a5
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2026-03-12 14:43:28 +0100

    Fix build

--- konfigyr-frontend/test/helpers/mocks.ts
+++ konfigyr-frontend/test/helpers/mocks.ts
@@ -1,6 +1,17 @@
+import * as profiles from './mocks/profile';
+import * as services from './mocks/services';
+
 export * as accounts from './mocks/account';
 export * as kms from './mocks/kms';
 export * as namespaces from './mocks/namespace';
 export * as applications from './mocks/application';
-export * as profiles from './mocks/profile';
-export * as services from './mocks/services';
+
+export const isValidProfile = (slug?: string | unknown) => (
+  slug === profiles.development.slug || slug === profiles.staging.slug || slug === profiles.deprecated.slug
+);
+
+export const isValidService = (slug?: string | unknown) => (
+  slug === services.konfigyrApi.slug || slug === services.konfigyrId.slug
+);
+
+export { profiles, services };
